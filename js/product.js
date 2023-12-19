a2a7c619ca34011428b4039a2f7bb65f66602bd7
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-12-19 21:49:00 +0500

    Add product cards

--- js/product.js
+++ js/product.js
@@ -1,5 +1,6 @@
-let id = 0;
+import { products } from '../utils/products.js';
 
+let id = 0;
 function handleAddProduct() {
   // step 03 & 04: Get input field values
   const name = document.getElementById("productName").value;
@@ -24,3 +25,21 @@ function handleAddProduct() {
   document.getElementById("price").value = "";
   document.getElementById("quantity").value = "";
 }
+
+// Add onclick event listener to add button
+document.getElementById("addButton").addEventListener("click", handleAddProduct);
+
+// For Each Loop
+// Render Cards
+products.forEach(function(product){
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML += `<div class="card mt-5" style="width: 22%;">
+  <img class="card-img-top" src=${product.image} alt="Card image cap" style="height: 150px; object-fit: cover;">
+  <div class="card-body">
+    <h5 class="card-title">${product.name}</h5>
+    <p class="card-text">${product.description}</p>
+    <a href="#" class="btn btn-primary">Learn more...</a>
+  </div>
+</div>`
+});
+
---
+++ utils/products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        id: 1,
+        name: "MacBook",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 240000,
+        image: "https://www.counterpointresearch.com/wp-content/uploads/2021/02/counterpoint-macbook-air-m1-first-impressions-lead.jpg"
+    },
+    {
+        id: 2,
+        name: "MacBook Pro",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 540000,
+        image: "https://photos5.appleinsider.com/gallery/45240-88149-The-new-MacBook-Pro-16-inch-xl.jpg"
+    },
+    {
+        id: 3,
+        name: "MacBook Pro",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 540000,
+        image: "https://photos5.appleinsider.com/gallery/45240-88149-The-new-MacBook-Pro-16-inch-xl.jpg"
+    },
+    {
+        id: 4,
+        name: "MacBook Pro",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 540000,
+        image: "https://photos5.appleinsider.com/gallery/45240-88149-The-new-MacBook-Pro-16-inch-xl.jpg"
+    },
+    {
+        id: 5,
+        name: "MacBook",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 240000,
+        image: "https://www.counterpointresearch.com/wp-content/uploads/2021/02/counterpoint-macbook-air-m1-first-impressions-lead.jpg"
+    },
+    {
+        id: 6,
+        name: "MacBook Pro",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 540000,
+        image: "https://photos5.appleinsider.com/gallery/45240-88149-The-new-MacBook-Pro-16-inch-xl.jpg"
+    },
+    {
+        id: 7,
+        name: "MacBook Pro",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 540000,
+        image: "https://photos5.appleinsider.com/gallery/45240-88149-The-new-MacBook-Pro-16-inch-xl.jpg"
+    },
+    {
+        id: 8,
+        name: "MacBook Pro",
+        description: "Here is the macbook Air M1 with 8GB RAM and its performance is very good. It's best choice for developers",
+        price: 540000,
+        image: "https://photos5.appleinsider.com/gallery/45240-88149-The-new-MacBook-Pro-16-inch-xl.jpg"
+    }
+];
+
+export { products };
